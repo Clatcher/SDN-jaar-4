fc85b7c84532ddcbdbe2c816ab3f21e17e0560d2
Clatcher <50823369+Clatcher@users.noreply.github.com>
2025-10-03 11:13:30 +0200

    Create simple_vlan.py

---
+++ simple_vlan.py
@@ -0,0 +1,34 @@
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.node import  RemoteController
+from mininet.cli import CLI
+from mininet.log import setLogLevel
+
+
+class SimpleVLANTopo(Topo):
+    def build(self):
+        #switches
+        s1 = self.addSwitch('s1')
+
+
+        #hosts (employees, guests, management)
+        h1 = self.addHost('h1', mac='00:00:00:00:00:01') # employee
+        h2 = self.addHost('h2', mac='00:00:00:00:00:02') # guest
+        h3 = self.addHost('h3', mac='00:00:00:00:00:03') # management
+
+        #switch koppelnen
+        self.addLink(h1, s1, port2=1) # employee
+        self.addLink(h2, s1, port2=2) # guest
+        self.addLink(h3, s1, port2=3) # management
+
+def run():
+    topo = SimpleVLANTopo()
+    net = Mininet(topo=topo, controller=RemoteController)
+    net.start()
+    print("Network is up and running.")
+    CLI(net)
+    net.stop()
+
+if __name__ == '__main__':
+    setLogLevel('info')
+    run()
