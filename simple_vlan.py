1e89dcd3f8ee895bd7e39c6a521d86c26393e465
Clatcher <erikkruize3410@gmail.com>
2025-10-13 16:57:58 +0200

    Gebouw B + DarkFiber

--- simple_vlan.py
+++ simple_vlan.py
@@ -7,34 +7,30 @@ from mininet.log import setLogLevel
 
 class SimpleVLANTopo(Topo):
     def build(self):
-        #switches
-        s1 = self.addSwitch('s1')
+        # switches (A en B)
+        s1 = self.addSwitch('s1')           # Gebouw A
+        s2 = self.addSwitch('s2')           # Gebouw B
 
+        # hosts in Gebouw A (1 per VLAN)
+        h1 = self.addHost('h1', mac='00:00:00:00:00:01')  # employee A
+        h2 = self.addHost('h2', mac='00:00:00:00:00:02')  # guest A
+        h3 = self.addHost('h3', mac='00:00:00:00:00:03')  # management A
 
-        #hosts (employees, guests, management)
-        h1 = self.addHost('h1', mac='00:00:00:00:00:01') # employee
-        h2 = self.addHost('h2', mac='00:00:00:00:00:02') # guest
-        h3 = self.addHost('h3', mac='00:00:00:00:00:03') # management
+        # hosts in Gebouw B (1 per VLAN)
+        h4 = self.addHost('h4', mac='00:00:00:00:00:11')  # employee B
+        h5 = self.addHost('h5', mac='00:00:00:00:00:12')  # guest B
+        h6 = self.addHost('h6', mac='00:00:00:00:00:13')  # management B
 
-        #switch koppellen
-        self.addLink(h1, s1, port2=1) # employee
-        self.addLink(h2, s1, port2=2) # guest
-        self.addLink(h3, s1, port2=3) # management
+        # access links (untagged aan hostzijde)
+        self.addLink(h1, s1, port2=1)  # A employee
+        self.addLink(h2, s1, port2=2)  # A guest
+        self.addLink(h3, s1, port2=3)  # A management
 
-def run():
-    topo = SimpleVLANTopo()
-    net = Mininet(topo=topo, controller=RemoteController)
-    net.start()
-    print("Network is up and running.")
-    CLI(net)
+        self.addLink(h4, s2, port2=1)  # B employee
+        self.addLink(h5, s2, port2=2)  # B guest
+        self.addLink(h6, s2, port2=3)  # B management
 
-    net.stop()
-
-if __name__ == '__main__':
-    setLogLevel('info')
-    run()
-
-topos = {
-    'simple_vlan': SimpleVLANTopo
-}
-
+        # darkfiber trunk tussen centrale patchkasten (s1<->s2)
+        # Gebruik poort 10 aan beide kanten voor de trunk
+        self.addLink(s1, s2, port1=10, port2=10)
+        # VLAN tagging wordt afgehandeld door de controller (OVS switch)
