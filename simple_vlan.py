48e68c0144f6a2faee400796e7f9933147ac7b68
Clatcher <erikkruize3410@gmail.com>
2025-10-14 23:52:56 +0200

    veel gezeik

--- simple_vlan.py
+++ simple_vlan.py
@@ -1,9 +1,8 @@
 from mininet.topo import Topo
 from mininet.net import Mininet
-from mininet.node import  RemoteController
+from mininet.node import RemoteController
 from mininet.cli import CLI
 from mininet.log import setLogLevel
-
 
 class SimpleVLANTopo(Topo):
     def build(self):
@@ -23,30 +22,44 @@ class SimpleVLANTopo(Topo):
         h6 = self.addHost('h6', mac='00:00:00:00:00:13')  # management B
         h8 = self.addHost('h8', mac='00:00:00:00:00:14')  # ISP B
         
-        
         # access links Gebouw A
-        self.addLink(h1, s1, port2=1)  # A employee
-        self.addLink(h2, s1, port2=2)  # A guest
-        self.addLink(h3, s1, port2=3)  # A management
-        self.addLink(h7, s1, port2=20)  # A ISP
-        
-        # access links Gebouw B
-        self.addLink(h4, s2, port2=1)  # B employee
-        self.addLink(h5, s2, port2=2)  # B guest
-        self.addLink(h6, s2, port2=3)  # B management
-        self.addLink(h8, s2, port2=20)  # B ISP
+        self.addLink(h1, s1, port2=1)
+        self.addLink(h2, s1, port2=2)
+        self.addLink(h3, s1, port2=3)
+        self.addLink(h7, s1, port2=20)
 
-        # darkfiber trunk tussen centrale patchkasten (s1<->s2)
-        # Gebruik poort 10 aan beide kanten voor de
-        self.addLink(s1, s2, port1=10, port2=10)  # trunk link
+        # access links Gebouw B
+        self.addLink(h4, s2, port2=1)
+        self.addLink(h5, s2, port2=2)
+        self.addLink(h6, s2, port2=3)
+        self.addLink(h8, s2, port2=20)
+
+        # trunk tussen s1 en s2
+        self.addLink(s1, s2, port1=10, port2=10)
 
 def run():
     topo = SimpleVLANTopo()
     net = Mininet(topo=topo, controller=RemoteController)
     net.start()
-    print("Network is up and running.")
-    CLI(net)
 
+    # Configureer IP’s voor ISP hosts
+    h7 = net.get('h7')
+    h8 = net.get('h8')
+
+    h7.cmd('ip addr flush dev h7-eth0')
+    h8.cmd('ip addr flush dev h8-eth0')
+    h7.cmd('ip addr add 203.0.113.2/28 dev h7-eth0')
+    h8.cmd('ip addr add 203.0.113.3/28 dev h8-eth0')
+
+    # Start VRRP met keepalived
+    h7.cmd('keepalived -n -l -f /tmp/keepalived-h7.conf &')
+    h8.cmd('keepalived -n -l -f /tmp/keepalived-h8.conf &')
+
+    # Wacht even zodat VRRP zich kan vestigen
+    net.waitConnected(timeout=3)
+
+    print("✅ Network is up and running.")
+    CLI(net)
     net.stop()
 
 if __name__ == '__main__':
